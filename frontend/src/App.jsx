50a9fe31fd75f4852f54dc793ff1d077a4a5cb12
Ralph-Dulla-23 <rdulla_22000000260@uic.edu.ph>
2023-10-18 21:11:43 +0800

    yess

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,22 +6,24 @@ import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
+
 function App() {
   return (
     <>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
       <div>
         <header className="header">
-          <a href="#" className='logo'>VSP</a>
-          <div style={{ backgroundImage: "url(Assets/Backgorund.png)" }}>
-              
-          </div>
-          <nav  style></nav>
+          <a className='logo'>VSP Laundry Shop</a>
+
+          <label htmlFor="" className="icons"><i class="fa fa-bars" aria-hidden="true"></i></label>
+        
+          <nav className='navbar'>
             <a href="#">HOME</a>
             <a href="#">ABOUT</a>
             <a href="#">SERVICES</a>
             <a href="#">INQUIRY</a>
             <a href="#">ACCOUNT</a>
-
+          </nav>
         </header>
         
       </div>
