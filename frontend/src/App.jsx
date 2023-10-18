3cdefaf1cb1c630f76a883f3226fa5d1bc2b6da3
Ralph-Dulla-23 <rdulla_22000000260@uic.edu.ph>
2023-10-18 21:55:00 +0800

    idk unsaon sa baba

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,22 +11,27 @@ function App() {
   return (
     <>
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
-      <div>
+      <div className='Content1'>
         <header className="header">
           <a className='logo'>VSP Laundry Shop</a>
 
           <label htmlFor="" className="icons"><i class="fa fa-bars" aria-hidden="true"></i></label>
-        
+        <div>
           <nav className='navbar'>
             <a href="#">HOME</a>
             <a href="#">ABOUT</a>
             <a href="#">SERVICES</a>
             <a href="#">INQUIRY</a>
             <a href="#">ACCOUNT</a>
-          </nav>
+          </nav> </div>
         </header>
-        
+       
       </div>
+      <div className='content2'>
+      <section className="Services">
+          <h1 id="Services">Services</h1>
+          <h2>hello</h2>
+      </section></div>
     </>
   )
 }
