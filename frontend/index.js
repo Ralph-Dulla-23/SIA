e23cd7d978b3686d16af7be77ac2095cd0869a2e
rambuTAN03 <142664911+rambuTAN03@users.noreply.github.com>
2023-11-07 14:13:34 +0800

    Changing console.log for backend

--- frontend/index.js
+++ frontend/index.js
@@ -2,11 +2,6 @@
 //fetching data
 fetch("http://localhost:3000/user")
     .then(res => res.json())
-    .then(json => {
-        json.map(data =>{
-            console.log(data)
-        })
-    })
-
+    .then(json => console.log(json))
 
 
