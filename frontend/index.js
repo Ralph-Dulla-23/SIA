073a3a363f0d5ac7e103fbd8b4c7f6b06571601e
rambuTAN03 <142664911+rambuTAN03@users.noreply.github.com>
2023-11-07 14:04:03 +0800

    backend with dummy data

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+
+//fetching data
+fetch("http://localhost:3000/user")
+    .then(res => res.json())
+    .then(json => {
+        json.map(data =>{
+            console.log(data)
+        })
+    })
+
+
+
