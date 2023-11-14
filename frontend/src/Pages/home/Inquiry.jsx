47198b68011f2984b16d989e8714066f1b901fdc
Ralph-Dulla-23 <rdulla_22000000260@uic.edu.ph>
2023-11-14 23:45:54 +0800

    revamped inquiry

--- frontend/src/Pages/home/Inquiry.jsx
+++ frontend/src/Pages/home/Inquiry.jsx
@@ -14,14 +14,14 @@ function Inquiry() {
             <NavBarAdmin />
 
             <div className='content1' style={{ marginTop: '2.9rem' }}>
-
+           
                 <div className="inquiry">
 
                     <div className='allChats'>
 
                         <span className="p-input-icon-left" style={{ width: '100%', position: 'absolute', margin: '1em 1.3em' }}>
                             <i className="pi pi-search" />
-                            <InputText placeholder="Search" />
+                            <InputText className="p-input-icon-left" placeholder="Search" />
                         </span>
 
                         <table style={{ height: 'inherit', width: '100%' }}>
@@ -40,8 +40,8 @@ function Inquiry() {
                         </div>
 
                         <div className='inputs'>
-                            <textarea name="" id="" cols="30" rows="10"></textarea>
-                            <Button label='Send' />
+                            <textarea  name="" id="" cols="30" rows="10"></textarea>
+                            <Button  label='Send' />
                         </div>
 
                     </div>
