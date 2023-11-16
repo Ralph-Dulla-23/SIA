6f87d6d9689ba2cc5765175203d196483077fc38
rambuTAN03 <142664911+rambuTAN03@users.noreply.github.com>
2023-11-16 23:13:05 +0800

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,10 @@
 
 //fetching data
 fetch("http://localhost:3000/user")
-    .then(res => res.json())
-    .then(json => console.log(json))
-
-
+    .then(res =>res.json())
+    .then(json => {
+        json.map(data => {
+            console.log(data)
+        })
+    })
+    
